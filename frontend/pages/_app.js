a659d736691b00bb94b497957bb7840dc1aa1fc3
Jinui You <baram991103@gmail.com>
2021-08-26 14:30:41 +0900

    :art: Make page structure

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+export default ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Meta Link</title>
+        <meta name="description" content="This is metalink from tophat" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
 
-export default MyApp
+      <Component {...pageProps} />
+
+      <Footer/>
+    </>
+  )
+};
