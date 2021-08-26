62fa9ec8551b01cd21f3aa796b1d5b20cb1eed04
ChangHoon Sung <changhoon.dev@gmail.com>
2021-08-26 06:34:59 +0000

    Update Article model

--- metalink/board/models.py
+++ metalink/board/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-from django.db.models.fields import CharField
 
 # Create your models here.
 
 
 class Article(models.Model):
-    id = models.BigIntegerField()
+    id = models.AutoField(primary_key=True)
     nickname = models.CharField(max_length=45)
     password = models.CharField(max_length=128)
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.TimeField(auto_now_add=True)
+    invite_url = models.URLField(max_length=255)
 
 
 class categories(models.Model):
-    id = models.BigIntegerField()
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
