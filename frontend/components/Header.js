7522443ce3874bd8880d643546629295c766e913
Jinui You <baram991103@gmail.com>
2021-08-26 14:46:15 +0900

    :lipstick: Add header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,5 +1,35 @@
+import styled from 'styled-components'
+
+const Header = styled.div`
+  display: flex;
+  background-color: #0070f3;
+  width: 100%;
+  height: 80px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const Name = styled.strong`
+  line-height: 80px;
+  font-size: 24px;
+  color: white;
+`;
+
+const WriteButton = styled.button`
+
+`;
+
 export default () => {
   return (
-    <></>
+    <Header>
+      <Name>
+        Metalink
+      </Name>
+
+      <WriteButton>
+        글쓰기
+      </WriteButton>
+    </Header>
   );
 };
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/reset.css';
 import Head from 'next/head'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
