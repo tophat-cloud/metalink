b85f2d5116e4c1ff26180791a98a515dc8f056fb
ChangHoon Sung <changhoon.dev@gmail.com>
2021-08-26 09:49:39 +0000

    Add Category

--- metalink/board/models.py
+++ metalink/board/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+
+
 class Article(models.Model):
     id = models.AutoField(primary_key=True)
     nickname = models.CharField(max_length=45)
@@ -10,9 +15,5 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     invite_url = models.URLField(max_length=255)
-
-
-class categories(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=45)
--- metalink/board/serializers.py
+++ metalink/board/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 
-from .models import Article
+from .models import Article, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
 
 class ArticleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Article
-        fields = ('id', 'nickname', 'title', 'content', 'invite_url', 'created_at')
+        fields = ('id', 'nickname', 'title', 'content', 'invite_url')
+        read_only_fields = ('category', 'created_at')
+    
+    def to_representation(self, instance):
+        self.fields['category'] =  CategorySerializer(read_only=True)
+        return super(ArticleSerializer, self).to_representation(instance)
 
 
 class CreateArticleSerializer(serializers.ModelSerializer):
