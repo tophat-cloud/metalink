2b19331631191f4db55f528c047bf43f7bebaf35
Jinui You <baram991103@gmail.com>
2021-08-26 15:21:53 +0900

    :sparkles: Add category

--- frontend/pages/posts/write/index.js
+++ frontend/pages/posts/write/index.js
@@ -8,8 +8,15 @@ const Wrapper = styled.div`
   padding: 64px 0;
 `;
 
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 const TitleInput = styled.input`
   width: 100%;
+  height: 44px;
   padding: 12px;
   border-radius: 8px;
   border: 1px solid #0070f3;
@@ -20,6 +27,12 @@ const TitleInput = styled.input`
   ::placeholder {
     color: #0070f3;
   }
+`;
+
+const SelectBox = styled.select`
+  height: 44px;
+  margin-left: 12px;
+  padding: 0 4px;
 `;
 
 const LinkInput = styled.input`
@@ -50,8 +63,6 @@ const ContentInput = styled.textarea`
   }
 `;
 
-
-
 const SubmitButton = styled.button`
   display: flex;
   width: 100%;
@@ -61,18 +72,36 @@ const SubmitButton = styled.button`
   justify-content: center;
   align-items: center;
   color: white;
+  background: lightgray;
 `;
 
 
 export default () => {
+  const categories = [
+    '카테고리1',
+    '카테고리2',
+    '카테고리3',
+    '카테고리4',
+  ]
+
   return (
     <>
       <h1>게시글 작성</h1>
       <Wrapper>
 
-        <TitleInput
-          placeholder="제목을 입력해주세요."
-        />
+        <TitleWrapper>
+          <TitleInput
+            placeholder="제목을 입력해주세요."
+          />
+
+          <SelectBox>
+            {
+              categories.map(v => (
+                <option>{v}</option>
+              ))
+            }
+          </SelectBox>
+        </TitleWrapper>
 
         <LinkInput
           placeholder="링크를 입력해주세요."
